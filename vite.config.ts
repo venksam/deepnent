21a35e2da0da8c025fdd086c61e0011b4d0b3405
venksam <147238691+venksam@users.noreply.github.com>
2026-03-18 16:12:20 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+  worker: {
+    format: 'es',
+  },
+  optimizeDeps: {
+    exclude: ['clipper-lib'],
+  },
+})
